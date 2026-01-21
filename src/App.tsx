0501a5d70ae31c43611948c28851062aac8d853e
Carolina Figueiredo <carolinasfigueiredo@hotmail.com>
2026-01-21 16:45:53 -0300

    Construção dos Componentes CardTema e ListaTemas

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Footer from './components/footer/Footer'
 import Navbar from './components/navbar/Navbar'
-import Home from './pages/home/Home'
-import Cadastro from './pages/cadastro/Cadastro'
-import Login from './pages/login/Login'
+import ListaTemas from './components/tema/listatemas/ListaTemas'
 import { AuthProvider } from './contexts/AuthContext'
+import Cadastro from './pages/cadastro/Cadastro'
+import Home from './pages/home/Home'
+import Login from './pages/login/Login'
 
 function App() {
 	return (
 		<>
 			<AuthProvider>
-			<BrowserRouter>
-				<Navbar />
-				<div className="min-h-[80vh]">
-					<Routes>
-						<Route path="/" element={<Login />} />
-						<Route path="/home"	element={<Home />} />
-						<Route path="/cadastro" element={<Cadastro />} />
-					</Routes>
-				</div>
-				<Footer />
-			</BrowserRouter>
+				<BrowserRouter>
+					<Navbar />
+					<div className="min-h-[80vh]">
+						<Routes>
+							<Route path="/" element={<Login />} />
+							<Route path="/home" element={<Home />} />
+							<Route path="/cadastro"	element={<Cadastro />}/>
+							<Route path="/temas" element={<ListaTemas />} />
+						</Routes>
+					</div>
+					<Footer />
+				</BrowserRouter>
 			</AuthProvider>
 		</>
 	)
